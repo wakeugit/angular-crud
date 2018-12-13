50bd8e890ac6430601af56ab3740da9b80235804
Wilfried Wakeu <wakeuwilfried@gmail.com>
2018-12-13 11:30:02 +0100

    build managment onfiguration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,10 +4,11 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      //require('karma-chrome-launcher'),
+      
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
